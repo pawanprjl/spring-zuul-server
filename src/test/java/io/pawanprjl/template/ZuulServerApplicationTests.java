efe5356cb0e05ecc67b009ac4d1529c19cc3eaa0
pawanprjl <pawanparajuli8@gmail.com>
2022-02-23 11:40:07 +0545

    initial commit

---
+++ src/test/java/io/pawanprjl/template/ZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pawanprjl.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
